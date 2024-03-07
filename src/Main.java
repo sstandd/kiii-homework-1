478cfc8f3be14452128d04f16ea148e2eef4e29a
stanchevskid <101475851+stanchevskid@users.noreply.github.com>
2024-03-07 23:49:26 +0100

    Added character printing function.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 package src;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IntStream.range(1, 10).boxed()
+                .forEach(element -> {
+                    IntStream.range(1, element)
+                            .boxed()
+                            .forEach(integer -> System.out.print("*"));
+                    System.out.println();
+                });
     }
 }
